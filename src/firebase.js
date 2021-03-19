ae9dea871fe2c930f83b95ea6b16c61bb3ba1ef2
rgmuna <rgmuna@gmail.com>
2021-03-18 20:55:11 -0700

    adding hosting and firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey           : "AIzaSyC0ySVhnpMO9Ls2qtgOVmXJZCDSqNGz51A",
+  authDomain       : "kirsten-app.firebaseapp.com",
+  databaseURL      : "https://kirsten-app-default-rtdb.firebaseio.com",
+  projectId        : "kirsten-app",
+  storageBucket    : "kirsten-app.appspot.com",
+  messagingSenderId: "619410044366",
+  appId            : "1:619410044366:web:50c8118274f8f5d797679f",
+  measurementId    : "G-0XV0FQKMLY"
+});
+
+export default firebase;
